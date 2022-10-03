50b4d58fa36389ca95f8abb6cb13ef3bae273648
Jayesh Singh <77924206+JARASI2003@users.noreply.github.com>
2022-10-03 13:32:08 +0530

    Add files via upload

---
+++ project/src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name    : sonarqube-jenkin Pipeline
+ * Developer       : Vishal Badgujar
+ * Version         : 1.0.0
+ * Date            : 29/07/2021
+ * Time            : 11:28 PM
+ * Description     :
+ **/
+
+
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
+    public void notCovered() {
+        System.out.println("This method is not covered by unit tests");
+    }
+
+
+}
---
+++ project/src/test/java/HelloWorldTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * Project Name    : sonarqube-jenkins Pipeline
+ * Developer       : Vishal Badgujar
+ * Version         : 1.0.0
+ * Date            : 29/7/2021
+ * Time            : 11:59 AM
+ * Description     :
+ **/
+
+
+public class HelloWorldTest {
+
+    @Test
+    public void sayHello() {
+        new HelloWorld().sayHello();
+    }
+
+
+}
